85ae96341be19fba311e50e273af83386df76021
James Waples <james@wapl.es>
2024-06-30 12:58:04 +0100

    Smaller debug tool to test thick line seed * 256

---
+++ debug-tools/examples/thick-line-mul-256.rs
@@ -0,0 +1,192 @@
+//! Testing perpendicular seed line for thick lines using the 256 mul trick as per
+//! <https://computergraphics.stackexchange.com/a/10675>.
+//!
+//! Copied from `line-no-aa-parallel.rs`.
+
+use embedded_graphics::{
+    geometry::PointExt, mock_display::MockDisplay, pixelcolor::Rgb888, prelude::*, primitives::Line,
+};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+use framework::prelude::*;
+
+#[derive(Debug, Clone, Copy)]
+struct MajorMinor<T> {
+    major: T,
+    minor: T,
+}
+
+impl<T> MajorMinor<T> {
+    fn new(major: T, minor: T) -> Self {
+        Self { major, minor }
+    }
+}
+
+fn thickline(
+    display: &mut impl DrawTarget<Color = Rgb888, Error = std::convert::Infallible>,
+    line: Line,
+    width: i32,
+) -> Result<(), std::convert::Infallible> {
+    if width == 0 {
+        return Ok(());
+    }
+
+    let non_mul_line = line;
+
+    let mut seed_line = line.perpendicular();
+
+    seed_line.start.x *= 256;
+    seed_line.end.x *= 256;
+
+    // line.start.y *= 256;
+    // line.end.y *= 256;
+
+    let mut point_left = seed_line.start;
+    let mut point_right = seed_line.start;
+
+    let seed_line_delta = seed_line.delta();
+    let seed_line_direction = Point::new(
+        if seed_line_delta.x >= 0 { 1 } else { -1 },
+        if seed_line_delta.y >= 0 { 1 } else { -1 },
+    );
+
+    let (seed_line_delta, seed_line_step) = if seed_line_delta.y.abs() >= seed_line_delta.x.abs() {
+        (
+            MajorMinor::new(seed_line_delta.y, seed_line_delta.x),
+            MajorMinor::new(seed_line_direction.y_axis(), seed_line_direction.x_axis()),
+        )
+    } else {
+        (
+            MajorMinor::new(seed_line_delta.x, seed_line_delta.y),
+            MajorMinor::new(seed_line_direction.x_axis(), seed_line_direction.y_axis()),
+        )
+    };
+
+    // Don't draw line skeleton twice
+    point_right -= seed_line_step.major;
+
+    let dx = seed_line_delta.major.abs();
+    let dy = seed_line_delta.minor.abs();
+
+    let threshold = dx - 2 * dy;
+    let e_minor = -2 * dx;
+    let e_major = 2 * dy;
+    let mut seed_line_error = 0;
+    let mut seed_line_error_right = e_major;
+
+    // Subtract 1 if using AA so 1px wide lines are _only_ drawn with AA - no solid fill
+    let thickness_threshold = ((width - 1) * 2).pow(2) * non_mul_line.delta().length_squared();
+    // Add the first line drawn to the thickness. If this is left at zero, an extra line will be
+    // drawn as the lines are drawn before checking for thickness.
+    let mut thickness_accumulator = 2 * dx;
+
+    // Bias to one side of the line
+    // TODO: The current extents() function needs to respect this too, as well as stroke offset
+    let mut is_right = true;
+
+    println!("---");
+
+    // TODO
+    // while thickness_accumulator.pow(2) <= thickness_threshold {
+    for _ in 0..width {
+        let (point, inc, c, seed_line_error) = if is_right {
+            (
+                &mut point_right,
+                MajorMinor::new(-seed_line_step.major, -seed_line_step.minor),
+                // Rgb888::CSS_DARK_GOLDENROD,
+                Rgb888::CSS_SALMON,
+                &mut seed_line_error_right,
+            )
+        } else {
+            (
+                &mut point_left,
+                seed_line_step,
+                Rgb888::CSS_FOREST_GREEN,
+                &mut seed_line_error,
+            )
+        };
+
+        dbg!(&point);
+
+        Pixel(dbg!(Point::new(point.x >> 8, point.y)), c).draw(display)?;
+
+        if *seed_line_error > threshold {
+            *point += inc.minor;
+            *seed_line_error += e_minor;
+            thickness_accumulator += 2 * dy;
+        }
+
+        *point += inc.major;
+        *seed_line_error += e_major;
+        thickness_accumulator += 2 * dx;
+
+        is_right = !is_right;
+    }
+
+    Ok(())
+}
+
+struct LineDebug {
+    start: Point,
+    end: Point,
+    stroke_width: u32,
+}
+
+impl App for LineDebug {
+    type Color = Rgb888;
+    const DISPLAY_SIZE: Size = Size::new(200, 200);
+    // const DISPLAY_SIZE: Size = Size::new(64, 64);
+
+    fn new() -> Self {
+        let end = Point::new(
+            Self::DISPLAY_SIZE.width as i32 / 2,
+            Self::DISPLAY_SIZE.height as i32 / 2,
+        );
+        Self {
+            start: end + Point::new(10, 15),
+            end,
+            // end: start + Point::new(100, 0),
+            stroke_width: 10,
+        }
+    }
+
+    fn parameters(&mut self) -> Vec<Parameter> {
+        vec![
+            Parameter::new("start", &mut self.start),
+            Parameter::new("end", &mut self.end),
+            Parameter::new("stroke", &mut self.stroke_width),
+        ]
+    }
+
+    fn draw(
+        &self,
+        display: &mut SimulatorDisplay<Self::Color>,
+    ) -> Result<(), std::convert::Infallible> {
+        let Point { x: _x0, y: _y0 } = self.start;
+
+        // let width = 2 * self.stroke_width as i32 * f32::sqrt((dx * dx + dy * dy) as f32) as i32;
+        // let width = (self.stroke_width as i32).pow(2) * (dx * dx + dy * dy);
+        let width = self.stroke_width as i32;
+
+        let _mock_display: MockDisplay<Rgb888> = MockDisplay::new();
+
+        thickline(display, Line::new(self.start, self.end), width)?;
+
+        // let l = Line::new(self.start, self.end);
+
+        // l.into_styled(PrimitiveStyle::with_stroke(Rgb888::GREEN, width as u32))
+        //     .draw(display)?;
+
+        // l.perpendicular()
+        //     .into_styled(PrimitiveStyle::with_stroke(Rgb888::RED, 1))
+        //     .draw(&mut display.translated(Point::new(40, 40)))?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let settings = OutputSettingsBuilder::new().scale(5).build();
+    let window = Window::new("Line debugger", &settings);
+
+    LineDebug::run(window);
+}
